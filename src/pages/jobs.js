2c9e95c1bdf4e03d088787fc3b9a54fd684392f2
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-03-02 15:33:44 -0800

    added JobListToolbar component to job page

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,25 +1,26 @@
 import Head from "next/head";
+import { Box, Container } from "@mui/material";
 import { DashboardLayout } from "../components/dashboard-layout";
+import { EmployeeListToolbar } from "src/components/employee/employee-list-toolbar";
+import { JobListToolbar } from "src/components/job/job-list-toolbar";
 
 const Customers = () => (
   <>
     <Head>
       <title>Jobs | Material Kit</title>
     </Head>
-    {/* <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8,
-        }}
-      >
-        <Container maxWidth={false}>
-          <EmployeeListToolbar />
-          <Box sx={{ mt: 3 }}>
-            <CustomerListResults customers={customers} />
-          </Box>
-        </Container>
-      </Box> */}
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8,
+      }}
+    >
+      <Container maxWidth={false}>
+        <JobListToolbar />
+        <Box sx={{ mt: 3 }}>{/* <CustomerListResults customers={customers} /> */}</Box>
+      </Container>
+    </Box>
   </>
 );
 Customers.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;

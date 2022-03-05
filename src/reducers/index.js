0eef511e12acb68f2d5d84b15e82d716ffbc2213
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-03-04 21:07:59 -0800

    added an initial reducer function

---
+++ src/reducers/index.js
@@ -0,0 +1,47 @@
+import { FETCH_START, FETCH_ERROR, FETCH_SUCCESS, SET_ERROR, LOGIN_SUCCESS } from "../actions";
+
+export const initialState = {
+  isLoading: false,
+  errorMessage: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+
+      return {
+        ...state,
+        user: { ...action.payload },
+        isLoading: false,
+        errorMessage: "",
+      };
+    case FETCH_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: "",
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: action.payload,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;

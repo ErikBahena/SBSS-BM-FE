17815200474d901a6cdadd3f303746ff19864510
Erik Bahena <erikjbahena@gmail.com>
2022-03-30 22:26:52 -0700

    update to fallback be api url

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.BACKEND_URL || "https://small-business-manager.vercel.app/api";
+export const BACKEND_URL = process.env.BACKEND_URL || "https://sbss-be-db.herokuapp.com//api";
 
 export const ALERT_SHOW_TIME_IN_MS = 2000;

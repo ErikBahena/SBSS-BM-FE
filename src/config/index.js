c2fc4af5adecf2c19d6a7fbf08abc40150c758bb
Erik Bahena <erikjbahena@gmail.com>
2022-03-30 22:14:24 -0700

    fallback to backend_url env var

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.BACKEND_URL;
+export const BACKEND_URL = process.env.BACKEND_URL || "https://small-business-manager.vercel.app/api";
 
 export const ALERT_SHOW_TIME_IN_MS = 2000;
